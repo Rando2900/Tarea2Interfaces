6ab33afdfe2bd0423776adaf816ddb3c31766e96
Rando <128974075+Rando2900@users.noreply.github.com>
2024-10-14 09:07:30 +0200

    Add files via upload

---
+++ public/javascripts/script.js
@@ -0,0 +1,7 @@
+function openCanvas() {
+    document.getElementById("offCanvas").classList.add("open");
+}
+
+function closeCanvas() {
+    document.getElementById("offCanvas").classList.remove("open");
+}
